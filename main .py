273171b6aaadb24b819da477a8c6ccdd05d31854
harshpreet26 <harshmaur2004@gmail.com>
2025-10-07 17:09:45 -0600

    commit

---
+++ main .py
@@ -0,0 +1,46 @@
+import ollama
+
+def chat_with_qwen():
+    #simple chat bot with Qwen
+    """This is a multi
+    -line comment."""
+    print("quen 2 chatbot (type'quit'to exit)")
+    print("-" * 50)
+
+    conversation_history = []
+
+    while True:
+        #capture user input
+        user_input = input("You: ").strip()
+        if user_input.lower() in ['quit','exit']:
+            print("Goodbye!")
+            break
+
+        if not user_input:
+            continue 
+
+        #add user input to message history
+        conversation_history.append({
+            "role": "user",
+            "content": user_input
+        })  
+          
+        try:
+            #response from the Qwen model
+            response = ollama.chat(
+                model="qwen2:0.5b",
+                messages=conversation_history,
+            )
+
+            assistant_message = response['message']['content']
+
+            #add assistant response to message history
+            conversation_history.append({"role": "assistant","content": assistant_message})
+
+            print(f" \n Qwen: {assistant_message}")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    chat_with_qwen()
